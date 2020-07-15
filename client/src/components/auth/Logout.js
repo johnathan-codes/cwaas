5e46f94e92a1661dfdfd12560ede18ccf910e0a5
Andrej Šebeň <andrej.seben@millennium.sk>
2020-07-15 15:57:21 +0200

    logout functional component

--- client/src/components/auth/Logout.js
+++ client/src/components/auth/Logout.js
@@ -1,25 +1,19 @@
-import React, { Component, Fragment } from 'react';
-import { connect } from 'react-redux';
-import { logout } from '../../actions/authActions';
-import { NavLink } from 'reactstrap';
-import PropTypes from 'prop-types';
+import React, { Fragment } from 'react'
+import { connect } from 'react-redux'
+import { logout } from '../../actions/authActions'
+import { NavLink } from 'reactstrap'
 
-export class Logout extends Component {
-  static propTypes = {
-    logout: PropTypes.func.isRequired
-  };
-  render() {
-    return (
-      <Fragment>
-        <NavLink onClick={this.props.logout} href='#'>
-          Logout
-        </NavLink>
-      </Fragment>
-    );
-  }
+const Logout = ({ logout }) => {
+  return (
+    <Fragment>
+      <NavLink onClick={logout} href="#">
+        Logout
+      </NavLink>
+    </Fragment>
+  )
 }
 
 export default connect(
   null,
   { logout }
-)(Logout);
+)(Logout)
