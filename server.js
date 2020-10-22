1d1142fb0449df153d9cd9d25017b428f8b99af7
johnathan-codes <42937562+johnathan-codes@users.noreply.github.com>
2020-10-22 09:15:20 +0200

    upgrade yarn.lock

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const path = require('path');
+const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
 
-const app = express();
+const app = express()
 
-app.use(express.json());
+app.use(express.json())
 
-const db = process.env.DB;
+const db = process.env.DB
 
-mongoose.set('useFindAndModify', false);
+mongoose.set('useFindAndModify', false)
 mongoose
-  .connect(db, { useNewUrlParser: true, useCreateIndex: true })
+  .connect(db, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
   .then(() => console.log('Database connection successful.'))
-  .catch(err => console.log(err));
+  .catch((err) => console.log(err))
 
-app.use('/api/items', require('./routes/api/items'));
-app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/items', require('./routes/api/items'))
+app.use('/api/auth', require('./routes/api/auth'))
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static('client/build'))
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
 }
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000
 
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+app.listen(port, () => console.log(`Server is running on port ${port}`))
