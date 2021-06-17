8ca8f788e2b8a643280d0c5a9e16c99fc0b977eb
johnathan-codes <42937562+johnathan-codes@users.noreply.github.com>
2021-06-17 23:52:53 +0200

    yarn upgrade, process env

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,19 +1,19 @@
-const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken')
 
 function auth(req, res, next) {
-  const token = req.header('x-auth-token');
+  const token = req.header('x-auth-token')
 
   if (!token) {
-    return res.status(401).json({ msg: 'Unauthorized access' });
+    return res.status(401).json({ msg: 'Unauthorized access' })
   }
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT);
-    req.user = decoded;
-    next();
+    const decoded = jwt.verify(token, process.env.JWT)
+    req.user = decoded
+    next()
   } catch (error) {
-    res.status(400).json({ msg: 'Invalid token' });
+    res.status(400).json({ msg: 'Invalid token' })
   }
 }
 
-module.exports = auth;
+module.exports = auth
